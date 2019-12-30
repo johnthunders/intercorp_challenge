fb1264fb6a252e230281e05b1d5ac1df1a687ea4
johnthunders <jsotomayortrujillo@gmail.com>
2019-12-30 12:17:09 -0500

    Intercorp commit

--- src/ebay/automation_ebay.java
+++ src/ebay/automation_ebay.java
@@ -1,6 +1,9 @@
 package ebay;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.JavascriptExecutor;
@@ -51,7 +54,7 @@ public class automation_ebay {
 		//5. Imprimir el número de resultados        
         String results_shoes = driver.findElement
                 (By.xpath("//h1[@class='srp-controls__count-heading']")).getText();
-        System.out.println(results_shoes);
+        System.out.println(results_shoes+"\n");
 		
         //6. Ordenar el precio por ascendente
         Actions orderBy = new Actions(driver);
@@ -64,6 +67,8 @@ public class automation_ebay {
 		//7. Mantener el orden tomando los primeros 5 resultados
 		//8. Elige los primeros 5 productos con sus precios e imprímelos en la consola
 		
+		System.out.println("5 primeros productos y sus precios:");
+		
 		List<WebElement> list_text = driver.findElements(By.className("s-item__title"));
 		List<WebElement> list_price = driver.findElements(By.className("s-item__price"));
         
@@ -74,7 +79,53 @@ public class automation_ebay {
 			System.out.println(desc_pro.getText() + ": " + prec_pro.getText());
 			
 			}
+
+		//9. Elige los primeros 5 productos con sus precios e imprímelos en la consola
 			
+			System.out.println("Productos ordenado en orden alfabetico:");
+			
+			String result_desc_1 =
+			driver.findElement(By.xpath("//li[@id='srp-river-results-listing1']//a[@class='s-item__link']")).getText();
+			
+			String result_desc_2 =
+			driver.findElement(By.xpath("//li[@id='srp-river-results-listing2']//a[@class='s-item__link']")).getText();
+					
+			String result_desc_3 =
+			driver.findElement(By.xpath("//li[@id='srp-river-results-listing3']//a[@class='s-item__link']")).getText();
+					
+			String result_desc_4 =
+			driver.findElement(By.xpath("//li[@id='srp-river-results-listing4']//a[@class='s-item__link']")).getText();
+					
+			String result_desc_5 =
+			driver.findElement(By.xpath("//li[@id='srp-river-results-listing5']//a[@class='s-item__link']")).getText();
+					
+			List<String> slist = Arrays.asList(result_desc_1, result_desc_2, result_desc_3, result_desc_4, result_desc_5); 
+			List<String> sortedList = slist.stream().sorted().collect(Collectors.toList());
+			sortedList.forEach(System.out::println);	
+			
+			
+		//10. Ordena e imprime los productos por precio (descendente)
+			String result_price_1 =
+			driver.findElement(By.xpath("//li[@id='srp-river-results-listing1']//div[@class='s-item__details clearfix']//div[1]")).getText();
+					
+			String result_price_2 =
+			driver.findElement(By.xpath("//li[@id='srp-river-results-listing2']//div[@class='s-item__details clearfix']//div[1]")).getText();
+					
+			String result_price_3 =
+			driver.findElement(By.xpath("//li[@id='srp-river-results-listing3']//div[@class='s-item__details clearfix']//div[1]")).getText();
+					
+			String result_price_4 =
+			driver.findElement(By.xpath("//li[@id='srp-river-results-listing4']//div[@class='s-item__details clearfix']//div[1]")).getText();
+					
+			String result_price_5 =
+			driver.findElement(By.xpath("//li[@id='srp-river-results-listing5']//span[@class='s-item__price']")).getText();
+					
+			List<String> plist = Arrays.asList(result_price_1, result_price_2, result_price_3, result_price_4, result_price_5); 
+			Collections.sort(plist, Collections.reverseOrder());
+
+			for(String str: plist){
+				System.out.println(str);
+			}
 		}
 	}
 
